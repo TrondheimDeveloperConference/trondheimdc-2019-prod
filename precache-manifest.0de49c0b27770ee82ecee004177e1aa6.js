7fdffa76ff5fbbb4a849ba5df5b16ad987424d72
Travis <travic@travis-ci.org>
2019-10-26 21:53:52 +0000

    Travis build 125

---
+++ precache-manifest.0de49c0b27770ee82ecee004177e1aa6.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ab33ef607686e6aa6d5aa4572d5299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f85b3b97154b9c085bf9",
+    "url": "/static/css/main.df800c99.chunk.css"
+  },
+  {
+    "revision": "a9da16f7aae0459207ff",
+    "url": "/static/js/2.44785440.chunk.js"
+  },
+  {
+    "revision": "f85b3b97154b9c085bf9",
+    "url": "/static/js/main.d34e8a6e.chunk.js"
+  },
+  {
+    "revision": "3ae9dbb2eb3ede2de076",
+    "url": "/static/js/runtime-main.b3a942fc.js"
+  },
+  {
+    "revision": "6814785df3f20f70641ac2ef0b747c82",
+    "url": "/static/media/bekk.6814785d.svg"
+  },
+  {
+    "revision": "6b9e6d6bdc548e99eef5c7ce988c9ac8",
+    "url": "/static/media/bg.6b9e6d6b.png"
+  },
+  {
+    "revision": "7752a01532538a5127bf9f9a4bd2a3f9",
+    "url": "/static/media/bg2.7752a015.png"
+  },
+  {
+    "revision": "6b7d051922e55e5cd0cc2784d5a56574",
+    "url": "/static/media/bg_jumbotron.6b7d0519.jpg"
+  },
+  {
+    "revision": "a2efbf75a09fea37c24e4afed218477d",
+    "url": "/static/media/bouvet.a2efbf75.svg"
+  },
+  {
+    "revision": "17efa687d8746dbe51c6046199c62ff3",
+    "url": "/static/media/calendar.17efa687.svg"
+  },
+  {
+    "revision": "7232c9b6193ec9d39ecc0130eda6f069",
+    "url": "/static/media/centric.7232c9b6.svg"
+  },
+  {
+    "revision": "f1b7676322b3d803a8c5c9c85822e40b",
+    "url": "/static/media/cgi.f1b76763.svg"
+  },
+  {
+    "revision": "7941ef8539236f3c9a246d836ebeadc9",
+    "url": "/static/media/coc_background.7941ef85.png"
+  },
+  {
+    "revision": "9f90e12911d7e841d3e00d458f6b94c7",
+    "url": "/static/media/code_jumbotron.9f90e129.png"
+  },
+  {
+    "revision": "cf145a2ceff57c5f678820be6d68cca9",
+    "url": "/static/media/dataforeningen.cf145a2c.svg"
+  },
+  {
+    "revision": "7a97704d0279d5acb4ae6319e34fe7bb",
+    "url": "/static/media/dips.7a97704d.svg"
+  },
+  {
+    "revision": "46bb59058ba39a53b02a082fd30eb897",
+    "url": "/static/media/email.46bb5905.svg"
+  },
+  {
+    "revision": "9dc9ec7d8c59e0d6092d3de126b151bd",
+    "url": "/static/media/equinor.9dc9ec7d.svg"
+  },
+  {
+    "revision": "17bb8e1b4d95ca2e8214b4d7d0cea362",
+    "url": "/static/media/evry.17bb8e1b.svg"
+  },
+  {
+    "revision": "34e73505be53a83b3d32236dab04f124",
+    "url": "/static/media/facebook.34e73505.svg"
+  },
+  {
+    "revision": "6948c7caa92c8ffea957540696a6da50",
+    "url": "/static/media/fjelltopmedia.6948c7ca.png"
+  },
+  {
+    "revision": "d161672cec5f8e6044ee597529710715",
+    "url": "/static/media/glasspaper.d161672c.svg"
+  },
+  {
+    "revision": "597b1cb39bdf308f75efcdb6fc2ea102",
+    "url": "/static/media/hamburger.597b1cb3.svg"
+  },
+  {
+    "revision": "62bf223d2bb23b29a9afbb41d81fee8e",
+    "url": "/static/media/hamburger_closed.62bf223d.svg"
+  },
+  {
+    "revision": "01b7ea06a552892f2fe49d9a5738e574",
+    "url": "/static/media/index_01.01b7ea06.jpg"
+  },
+  {
+    "revision": "b303a4af7375a9a7455fa0cf5c0d090f",
+    "url": "/static/media/index_03.b303a4af.jpg"
+  },
+  {
+    "revision": "c63285cec67138ee7ff90b85034ba473",
+    "url": "/static/media/itema.c63285ce.png"
+  },
+  {
+    "revision": "afc3f62bebe62a4c31caacc5f9cea0ab",
+    "url": "/static/media/itsjefen.afc3f62b.png"
+  },
+  {
+    "revision": "89b343c52c0f6cb17e9ede78ea0b193e",
+    "url": "/static/media/ixda.89b343c5.svg"
+  },
+  {
+    "revision": "5703732e5c9962556a6ba7f7a1bc3400",
+    "url": "/static/media/javabin.5703732e.svg"
+  },
+  {
+    "revision": "61f9caafb8b8c7a7c2a39cc73fd8bdcd",
+    "url": "/static/media/kantega.61f9caaf.svg"
+  },
+  {
+    "revision": "3cc1c4e5a18cbc91decb0d4c5fccd67e",
+    "url": "/static/media/knowit.3cc1c4e5.svg"
+  },
+  {
+    "revision": "080be256851513f60f4fe1dc082107cc",
+    "url": "/static/media/lindbak.080be256.svg"
+  },
+  {
+    "revision": "da7c862d2e0cb0ea365be155cd3fc602",
+    "url": "/static/media/microsoft.da7c862d.svg"
+  },
+  {
+    "revision": "8fd6210c73a62091c882ed38fcc28a77",
+    "url": "/static/media/nnug.8fd6210c.svg"
+  },
+  {
+    "revision": "c60e0d1c66af442fdd581e2932789209",
+    "url": "/static/media/partners_jumbotron.c60e0d1c.png"
+  },
+  {
+    "revision": "ea7df5ab3fc7e54e2c69b3607cca2a96",
+    "url": "/static/media/programme_jumbotron.ea7df5ab.png"
+  },
+  {
+    "revision": "272d906475ee10c3d7903742633f069c",
+    "url": "/static/media/programutvikling.272d9064.svg"
+  },
+  {
+    "revision": "705373fff381416c3146d9403e1044ef",
+    "url": "/static/media/signicat.705373ff.svg"
+  },
+  {
+    "revision": "e9ea64cd264b5a5331da64250f025951",
+    "url": "/static/media/spillmakerlauget.e9ea64cd.svg"
+  },
+  {
+    "revision": "e17cf6249a8844cb04512859efcff5e3",
+    "url": "/static/media/sportradar.e17cf624.png"
+  },
+  {
+    "revision": "488a9d1ec7f159949dba4fb744275cdf",
+    "url": "/static/media/startups_jumbotron.488a9d1e.png"
+  },
+  {
+    "revision": "4c06b838e626f191b792dc35ad865844",
+    "url": "/static/media/sticos.4c06b838.svg"
+  },
+  {
+    "revision": "35cc8c1b3f87fe3ac935cc3fbdf6ce51",
+    "url": "/static/media/tdc_grayscale.35cc8c1b.png"
+  },
+  {
+    "revision": "4399c36b3f777c560f178fba62a0a183",
+    "url": "/static/media/tdc_jumbotron.4399c36b.png"
+  },
+  {
+    "revision": "e5680f2efafce5f99e145f26a6534cb9",
+    "url": "/static/media/ticket_jumbotron.e5680f2e.png"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/static/media/twitter.a1223de5.svg"
+  },
+  {
+    "revision": "633890605c4cd71d16f5c7950eded7cf",
+    "url": "/static/media/uninett.63389060.svg"
+  },
+  {
+    "revision": "28cee8769cefc9f2ce29877975d5e541",
+    "url": "/static/media/variant.28cee876.svg"
+  },
+  {
+    "revision": "2b4f8fe7c61f617567aa604f8a7a202f",
+    "url": "/static/media/webstep.2b4f8fe7.svg"
+  }
+]);
